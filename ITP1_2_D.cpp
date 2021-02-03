609cd0535ce4e1934962c6f86cc843740e13c77c
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-02-03 23:49:39 +0900

    AOJ 2021/02/03

---
+++ ITP1_2_D.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  int w,h,x,y,r;
+  cin >> w >> h >> x >> y >> r;
+  
+  if( (x - r) >= 0 && (x + r) <= w ) {
+    if( (y - r) >= 0 && (y + r) <= h ) {
+      cout << "Yes" << endl;
+    }
+    else {
+      cout << "No" << endl;
+    }  
+  }
+  
+  else {
+    cout << "No" << endl;
+  }
+    
+}
