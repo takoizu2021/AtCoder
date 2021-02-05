72acf1431c387d09dfd977833eae00dd819e3eba
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-02-05 22:19:45 +0900

    AOJ 2021/02/05

---
+++ ITP1_4_A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  
+  int a,b;
+  cin >> a >> b;
+  
+  cout << a / b << endl;
+  cout << a % b << endl;
+  
+  double f1 = 1.0 * a / b;
+  printf("%.5lf\n", f1);
+  
+}
