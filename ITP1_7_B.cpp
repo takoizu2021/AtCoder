840751b5493559abc9062937bbe7fd969e370afa
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-02-15 00:38:43 +0900

    AOJ 2021/02/15

---
+++ ITP1_7_B.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main() {
+  
+  int n,x;
+  
+  while(true) {
+  
+    cin >> n >> x;  
+    if (n == 0 && x == 0 ) {
+      break;
+    }
+    int count = 0;
+    
+    rep2(i,1,n + 1) {
+      rep2(j,i + 1,n + 1) {
+        rep2(k,j + 1,n + 1) {
+          
+          if(i + j + k == x) {
+            count ++;
+          }
+          
+        }
+      }
+    }
+    
+    cout << count << endl;    
+  }
+
+}
