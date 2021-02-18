efee1d4a2c7c403e72fc04fe1feb1b8b9603fc52
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-02-18 23:56:26 +0900

    ABC 2021/02/18

---
+++ ABC100_A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+
+int main() {
+ 
+  int a,b;
+  cin >> a >> b;
+  int n = 16;
+  
+  if(a > 8 || b > 8) {
+    cout << ":(" << endl;
+  }
+  else {
+    cout << "Yay!" << endl;
+  }
+  
+}
