6ecdb4e53142a04c200756db1a59d37231186d34
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-04-06 07:30:15 +0900

    2021/04/06

--- ABC088_B.cpp
+++ ABC088_B.cpp
@@ -2,32 +2,22 @@
 using namespace std;
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 #define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
-using ll = int64_t;
+#include <cmath> //数学関数
+using ll = long long;
 
 int main() {
   int n;
-  cin >> n;
+  cin>>n;
+  vector<int> ca(n);
+  rep(i,n) cin>>ca[i];
+  sort(ca.begin(), ca.end());
+  reverse(ca.begin(), ca.end());
   
-  vector<int> card(n);
-  
+  int a=0; int b=0;
   rep(i,n) {
-    cin >> card[i];
+    if(i == 0 || i % 2 == 0)
+      a += ca[i];
+    else b += ca[i];
   }
-  //数字が大きい順に配列をソート
-  sort(card.begin(), card.end());
-  reverse(card.begin(),card.end());
-  
-  int a = 0;
-  int b = 0;
-  
-  rep(i,n) {
-    if(i == 0 || i % 2 == 0) {
-      a += card[i];
-    }
-    else {
-      b += card[i];
-    }
-  }
-  
-  cout << a - b << endl;
+  cout<<a-b<<endl;
 }
