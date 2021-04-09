2fe0d2b89014a5a268d0ffb85f778416dd1f2925
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-04-09 23:33:16 +0900

    2021/04/09

---
+++ ABC076_B.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath> //”ŠwŠÖ”
+using ll = long long;
+
+int main() {
+  int n,k;
+  cin>>n>>k;
+  
+  int ans=1;
+  rep(i,n) {
+    if(ans > k) ans += k;
+    else ans *= 2;
+  }
+  cout<<ans<<endl;
+}
