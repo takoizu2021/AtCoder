6433b5ce04eeacefc31a94dc9f8c026e5629c2e6
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-05-01 11:44:17 +0900

    2021/05/01

---
+++ ABC122_A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath> //”ŠwŠÖ”
+using ll = long long;
+
+int main() {
+  char s;
+  cin>>s;
+  
+  if(s == 'A') cout<<'T'<<endl;
+  else if(s == 'T') cout<<'A'<<endl;
+  else if(s == 'C') cout<<'G'<<endl;
+  else if(s == 'G') cout<<'C'<<endl;
+}
