904ef7773409e9d3804647fb9a8295cba73c89d6
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-06-19 01:18:48 +0900

    2021/06/19

---
+++ ABC170_A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath> //”ŠwŠÖ”
+using ll = long long;
+
+int main() {
+  int a,b,c,d,e;
+  cin>>a>>b>>c>>d>>e;
+  
+  if(a == 0) cout<<1<<endl;
+  if(b == 0) cout<<2<<endl;
+  if(c == 0) cout<<3<<endl;
+  if(d == 0) cout<<4<<endl;
+  if(e == 0) cout<<5<<endl;
+  
+}
