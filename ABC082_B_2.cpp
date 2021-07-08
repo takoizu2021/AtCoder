7180377b7f256ec5559d917508c4fe64e06e08e1
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-07-09 07:29:49 +0900

    2021/07/09

---
+++ ABC082_B_2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath> //”ŠwŠÖ”
+using ll = long long;
+
+int main() {
+  string s,t;
+  cin>>s>>t;
+  
+  sort(s.begin(),s.end());
+  sort(t.begin(),t.end());
+  reverse(t.begin(),t.end());
+  
+  string ans = "No";
+  if(s<t) ans = "Yes";
+  
+  cout<<ans<<endl;
+    
+}
