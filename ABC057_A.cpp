d887b2f9463678ce84f7839d4caca4dc123ab9ee
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-07-16 23:54:11 +0900

    2021/07/16

---
+++ ABC057_A.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath> //”ŠwŠÖ”
+using ll = long long;
+
+int main() {
+  int a,b;
+  cin>>a>>b;
+  
+  int ans=a+b;
+  if(ans >= 24) ans -= 24;
+  
+  cout<<ans<<endl;
+}
