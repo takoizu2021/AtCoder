62ced060fc66f9a23eace256b0388e48b3b36b02
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-07-28 23:43:52 +0900

    2021/07/28

---
+++ ABC020_A_2.cpp
@@ -0,0 +1,17 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#include <cmath>
+using ll = long long;
+
+int main() {
+  int q;
+  cin>>q;
+  
+  string ans="chokudai";
+  if(q == 1) ans="ABC";
+  
+  cout<<ans<<endl;
+}
