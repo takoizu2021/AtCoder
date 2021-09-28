b25888fa7e799fa2b535c68af2449a529dd83c1e
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-09-29 03:37:26 +0900

    2021/09/29

---
+++ アルゴ式_整数論的アルゴリズムQ4.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#include <regex> // ³‹K•\Œ»‚ğæ‚èˆµ‚¤‚½‚ß‚Ìƒ‰ƒCƒuƒ‰ƒŠ
+using namespace std;
+const int inf = 1000000;
+
+bool chk (int x) {
+	if(x < 2) return false;
+	for(int i=2; i*i<x; ++i) {
+		if(x%i==0) return false;		
+	}
+	return true;
+}
+
+int main() {
+	int n;
+	cin>>n;
+	
+	while(!chk(n)) n--;
+
+	cout << n << endl;
+	return 0;
+}
