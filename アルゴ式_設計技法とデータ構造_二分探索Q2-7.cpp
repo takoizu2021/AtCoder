f9794dd145232b5a0c356b08da786ab0257fc7cf
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-10-13 07:31:18 +0900

    2021/10/13

--- アルゴ式_設計技法とデータ構造_二分探索Q2-7.cpp
+++ アルゴ式_設計技法とデータ構造_二分探索Q2-7.cpp
@@ -1,21 +1,23 @@
 #include <bits/stdc++.h>
 #include <regex> // ³‹K•\Œ»‚ðŽæ‚èˆµ‚¤‚½‚ß‚Ìƒ‰ƒCƒuƒ‰ƒŠ
 using namespace std;
-const long long inf = 10000000000000; //10^13
+const long long inf = 1000000000000000000; //10^18
+
+//f(x)‚ð‹‚ß‚éŠÖ”
+long long f(long long x) {
+    return x*(x+1)/2;
+}
 
 int main() {
 	int n; cin>>n;
-	vector<int> w(n);
-	for(int i=0; i<n; ++i) cin>>w[i];
-
-	vector<int> v = w;
-	sort(v.begin(), v.end());
+	vector<long long> x(n);
+	for(int i=0; i<n; ++i) cin>>x[i];
 
 	for(int i=0; i<n; ++i) {
-		int left=0, right=n-1;
+		long long left=0, right=2*1000000000;
 		while(left!=right) {
-			int mid = (left+right) / 2;
-			if(v[mid] >= w[i]) {
+			long long mid = (left+right) / 2;
+			if(f(mid) >= x[i]) {
 				right = mid;
 			}
 			else {
@@ -24,6 +26,5 @@ int main() {
 		}
 		cout<<left<<endl;
 	}
-
 	return 0;
 }
