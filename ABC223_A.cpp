6659e16eb76e1ecb0f433d80060f853a205036ed
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2021-10-17 23:51:20 +0900

    2021/10/17

---
+++ ABC223_A.cpp
@@ -0,0 +1,16 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#include <cmath>
+const int inf=1000000000; //10^9
+using ll = long long;
+
+int main() {
+  int x;
+  cin>>x;
+  
+  cout<<( x%100==0&&x>=100 ? "Yes" : "No")<<endl;
+}
---
+++ ABC223_B.cpp
@@ -0,0 +1,26 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#include <cmath>
+const int inf=1000000000; //10^9
+using ll = long long;
+
+int main() {
+  string s;
+  cin>>s;
+  
+  int n = s.size();
+  string ss = s+s;
+  string mn=s,mx=s;
+  for(int i=0; i<n; ++i) {
+	string l = ss.substr(i,n);
+    mn = min(mn,l);
+    mx = max(mx,l);
+  }
+  
+  cout<<mn<<endl<<mx<<endl;
+  
+}
