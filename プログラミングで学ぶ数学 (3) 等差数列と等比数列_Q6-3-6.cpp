0e474a39320a0484d3b810fd4556a2f311ad7df1
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2022-01-12 06:17:44 +0900

    2022/01/12

---
+++ プログラミングで学ぶ数学 (3) 等差数列と等比数列_Q6-3-6.cpp
@@ -0,0 +1,17 @@
+#include bitsstdc++.h
+#include regex  ³‹K•\Œ»‚ğæ‚èˆµ‚¤‚½‚ß‚Ìƒ‰ƒCƒuƒ‰ƒŠ
+using namespace std;
+#define rep(i, n) for (int i = 0; i  (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i  (int)(n); i++)
+#define all(v) v.begin(), v.end()
+const long long inf = 10000000000000; 10^13
+using ll = long long;
+
+int main() {
+	ll ans = 1;
+	for(int i=0; i=20; ++i) {
+		if(i!=0) ans = ans2;
+		coutansendl;
+	}	
+	return 0;
+}
