25373ab882edb2bffec8dbd55788155771e96018
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2022-01-16 19:25:23 +0900

    2022/01/16

---
+++ アルゴ式_プログラミングで学ぶ数学 (4) 極限_Q6-4-2.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#include <regex> // ³‹K•\Œ»‚ğæ‚èˆµ‚¤‚½‚ß‚Ìƒ‰ƒCƒuƒ‰ƒŠ
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+const long long inf = 10000000000000; //10^13
+using ll = long long;
+
+int main() {
+	int a,b;
+	cin>>a>>b;
+
+	int ans;
+    if(a>b) ans=2;
+    else if(-1*b<a && b>=a) ans=1;
+    else ans=3;
+
+	cout << ans << endl;
+	return 0;
+}
---
+++ アルゴ式_プログラミングで学ぶ数学 (4) 極限_Q6-4-3.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#include <regex> // ³‹K•\Œ»‚ğæ‚èˆµ‚¤‚½‚ß‚Ìƒ‰ƒCƒuƒ‰ƒŠ
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+const long long inf = 10000000000000; //10^13
+using ll = long long;
+
+int main() {
+    int a,b;
+    cin>>a>>b;
+    if(b < 0) cout<<"-inf"<<endl;
+    else if(b==0) cout<<a<<endl;
+    else cout<<"inf"<<endl;
+	return 0;
+}
