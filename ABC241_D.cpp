4fa06162e4ea4977233cb451d0cd7683cb01ad3b
jun_kano-PC\jun_kano <hiagon200804@yahoo.co.jp>
2022-02-27 13:45:32 +0900

    2022/02/27

---
+++ ABC241_D.cpp
@@ -0,0 +1,69 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#include <cmath>
+const int inf=1000000000; //10^9
+using ll = long long;
+//Å‘åŒö–ñ” gcd(A,B)
+
+
+
+int main() {
+  int q;
+  cin>>q;
+  multiset<ll> s;
+  /*
+  int x = 10;
+  s.insert(x);
+  s.insert(x+1);
+  s.insert(x*2);
+  auto itr = s.end();
+  cout<<*itr<<endl;
+  */
+  rep(i,q) {
+    int val;
+    cin>>val;
+    
+    if(val==1) {
+      ll x;
+      cin>>x;
+      s.insert(x);
+    }
+    
+    if(val==2) {
+      ll x,k;
+      cin>>x>>k;
+      auto itr = s.upper_bound(x);      
+      while(itr!=s.begin()&&k) {
+        itr--;
+        k--;
+      }
+      if(k>0) cout<<-1<<endl;      
+      else cout<<*itr<<endl;
+      
+    }
+    
+    if(val==3) {
+      ll x,k;
+      cin>>x>>k;
+      auto itr = s.lower_bound(x);
+      
+      int cht = 0;
+      bool chk = false;
+      while(itr!=s.end()) {
+        cht++;
+        if(cht==k) {
+          cout<<*itr<<endl;
+          chk = true;
+          break;
+        }
+        itr++;
+      }
+      if(!chk) cout<<-1<<endl;
+    }
+  }
+  //*/
+}
