af9815a09bf197aa36ac312bda561f01946f056d
takoizu2021 <hiagon200804@yahoo.co.jp>
2022-05-22 23:44:31 +0900

    2022/05/22

---
+++ ABC252_A_2.cpp
@@ -0,0 +1,19 @@
+#define _GLIBCXX_DEBUG
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (int)(n); i++)
+#define rep2(i, s, n) for (int i = (s); i < (int)(n); i++)
+#define all(v) v.begin(), v.end()
+#include <cmath>
+const int inf=1000000000; //10^9
+const int mod=998244353;
+using ll = long long;
+//Å‘åŒö–ñ” gcd(A,B)
+
+int main() {
+  int n;
+  cin>>n;
+  
+  char s = n;
+  cout<<s<<endl;  
+}
